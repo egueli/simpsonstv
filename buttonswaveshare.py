e182c3fe6162124562afc642d62c8dbe46984d48
Enrico Gueli <enrico.gueli@gmail.com>
2024-02-03 17:49:42 +0100

    fix indentation; allow buttonswaveshare.py to be imported

--- buttonswaveshare.py
+++ buttonswaveshare.py
@@ -11,32 +11,34 @@ os.system('amixer sset "PCM" 0%')
 
 def turnOnScreen():
     os.system('raspi-gpio set 13 op a0')
-# power on screen
+    # power on screen
     os.system('sudo echo 0 | sudo tee /sys/class/backlight/rpi_backlight/bl_power')
-# change volume to 90%
-    os.system('amixer sset "PCM" 90%')
+    # change volume to 90%
+    os.system('amixer sset "PCM" 10%')
 
 
 def turnOffScreen():
     os.system('raspi-gpio set 13 ip')
-# power off screen
+    # power off screen
     os.system('sudo echo 1 | sudo tee /sys/class/backlight/rpi_backlight/bl_power')
-# change volume to 0%
+    # change volume to 0%
     os.system('amixer sset "PCM" 0%')
 
 
 turnOffScreen()
 screen_on = False
 
-while (True):
-    # If you are having and issue with the button doing the opposite of what you want
-    # IE Turns on when it should be off, change this line to:
-    # input = not GPIO.input(26)
-    input = GPIO.input(26)
-    if input != screen_on:
-        screen_on = input
-        if screen_on:
-            turnOnScreen()
-        else:
-            turnOffScreen()
-    time.sleep(0.3)
+if __name__ == '__main__':
+
+    while (True):
+        # If you are having and issue with the button doing the opposite of what you want
+        # IE Turns on when it should be off, change this line to:
+        # input = not GPIO.input(26)
+        input = GPIO.input(26)
+        if input != screen_on:
+            screen_on = input
+            if screen_on:
+                turnOnScreen()
+            else:
+                turnOffScreen()
+        time.sleep(0.3)
